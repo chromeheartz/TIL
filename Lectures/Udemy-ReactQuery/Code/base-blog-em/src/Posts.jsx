7cc955e5ca7c8d39fad38a57d480a7079b672920
bibiboy <barnesquiat@gmail.com>
2023-08-06 12:54:13 +0900

    [L][Udemy-RQ][C01] 04 - 05
    04. useQuery로 쿼리 생성하기
    05. 로딩상태와 에러상태 처리하기

--- Lectures/Udemy-ReactQuery/Code/base-blog-em/src/Posts.jsx
+++ Lectures/Udemy-ReactQuery/Code/base-blog-em/src/Posts.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useQuery } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
@@ -15,12 +16,20 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts);
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError) 
+    return (
+      <>
+        <h3>Oops, Something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
       <ul>
-        {data.map((post) => (
+        {data.map(post => (
           <li
             key={post.id}
             className="post-title"
