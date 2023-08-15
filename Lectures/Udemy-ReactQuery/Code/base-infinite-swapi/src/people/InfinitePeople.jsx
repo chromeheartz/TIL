5a0e41d33b72bcd86d6e1c0cc3849931603dcde1
bibiboy <barnesquiat@gmail.com>
2023-08-15 20:37:39 +0900

    [L][Udemy-RQ][C03] 15. useInfiniteQuery 호출 작성하기

--- Lectures/Udemy-ReactQuery/Code/base-infinite-swapi/src/people/InfinitePeople.jsx
+++ Lectures/Udemy-ReactQuery/Code/base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,6 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
-import { useQuery } from "react-query";
 import { Person } from "./Person";
+import { useInfiniteQuery } from "react-query";
 
 const initialUrl = "https://swapi.dev/api/people/";
 const fetchUrl = async (url) => {
@@ -9,20 +9,16 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  const { data, isLoading } = useQuery("infinity", () => fetchUrl(initialUrl));
-  console.log(data);
+  const { data: infiniteData, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    "people",
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined
+    }
+  );
 
-  if(isLoading) <h3>hello</h3>
-  
-  // return <InfiniteScroll />;
-  return (
-    <>
-      {
-        data.results.map((v, i) => {
-          return <Person data={v} />
-        })
-      }
-    </>
-  )
+  console.log(infiniteData);
+
+  // TODO: get data for InfiniteScroll via React Query
+  return <InfiniteScroll />;
 }
