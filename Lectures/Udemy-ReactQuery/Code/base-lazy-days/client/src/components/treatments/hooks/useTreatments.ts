7418d49abf9e7dc784640d6ca3ac6a68620e069b
bibiboy <barnesquiat@gmail.com>
2023-08-20 22:41:02 +0900

    [L][Udemy-RQ][C04] 20. Custom Hooks

--- Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -4,10 +4,10 @@ import { queryKeys } from '../../../react-query/constants';
 import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
   // TODO: get data from server via useQuery
