8ff894890a0a0af83fa854e7581f258e3e37d16b
bibiboy <barnesquiat@gmail.com>
2023-08-27 15:56:26 +0900

    [L][Udemy-RQ][C05] 26. 의존성 배열로서의 쿼리 키

--- Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
+++ Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import dayjs from 'dayjs';
 import { Dispatch, SetStateAction, useState } from 'react';
+import { useQuery } from 'react-query';
 
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
@@ -63,14 +64,20 @@ export function useAppointments(): UseAppointments {
   /** ****************** START 3: useQuery  ***************************** */
   // useQuery call for appointments for the current monthYear
 
-  // TODO: update with useQuery!
   // Notes:
   //    1. appointments is an AppointmentDateMap (object with days of month
   //       as properties, and arrays of appointments for that day as values)
   //
   //    2. The getAppointments query function needs monthYear.year and
   //       monthYear.month
-  const appointments = {};
+  const fallback = {};
+  const { data: appointments = fallback } = useQuery(
+    [queryKeys.appointments, monthYear.year, monthYear.month],
+    () => getAppointments(monthYear.year, monthYear.month),
+    {
+      keepPreviousData: true,
+    },
+  );
 
   /** ****************** END 3: useQuery  ******************************* */
 
