1e681205e51a93c2c68292e7ca7c706e10c35650
bibiboy <barnesquiat@gmail.com>
2023-09-03 19:21:59 +0900

    [L][Udemy-RQ][C06] 32. 리패치(Re-fetch) 옵션 업데이트

--- Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -13,11 +13,20 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+    staleTime: 600000, // 10 min
+    cacheTime: 900000, // 15 min
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+  });
   return data;
 }
 
 export function usePrefetchTreatments(): void {
   const queryClient = useQueryClient();
-  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments, {
+    staleTime: 600000, // 10 min
+    cacheTime: 900000, // 15 min
+  });
 }
