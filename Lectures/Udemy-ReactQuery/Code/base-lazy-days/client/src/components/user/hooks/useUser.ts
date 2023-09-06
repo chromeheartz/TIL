dddb6eab8b3e555d120cf41edfa7fef84edbd1a3
bibiboy <barnesquiat@gmail.com>
2023-09-07 07:22:18 +0900

    [L][Udemy-RQ][C07] 35. useAuth, useUser / useQuery

--- Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,4 +1,5 @@
 import { AxiosResponse } from 'axios';
+import { useQuery } from 'react-query';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
@@ -9,16 +10,16 @@ import {
   setStoredUser,
 } from '../../../user-storage';
 
-// async function getUser(user: User | null): Promise<User | null> {
-//   if (!user) return null;
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${user.id}`,
-//     {
-//       headers: getJWTHeader(user),
-//     },
-//   );
-//   return data.user;
-// }
+async function getUser(user: User | null): Promise<User | null> {
+  if (!user) return null;
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${user.id}`,
+    {
+      headers: getJWTHeader(user),
+    },
+  );
+  return data.user;
+}
 
 interface UseUser {
   user: User | null;
@@ -28,8 +29,7 @@ interface UseUser {
 
 export function useUser(): UseUser {
   // TODO: call useQuery to update user data from server
-  const user = null;
-
+  const { data: user } = useQuery(queryKeys.user, () => getUser(user));
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
     // TODO: update the user in the query cache
