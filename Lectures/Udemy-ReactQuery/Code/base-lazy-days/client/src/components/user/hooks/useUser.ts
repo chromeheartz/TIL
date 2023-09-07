6fafb77d2968c4b0647fbd1a59b2a79ae96f12df
bibiboy <barnesquiat@gmail.com>
2023-09-08 08:34:44 +0900

    [L][Udemy-RQ][C07] 36. React Query와 Auth 통합하기

--- Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,5 +1,5 @@
 import { AxiosResponse } from 'axios';
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
@@ -28,16 +28,16 @@ interface UseUser {
 }
 
 export function useUser(): UseUser {
-  // TODO: call useQuery to update user data from server
+  const queryClient = useQueryClient();
   const { data: user } = useQuery(queryKeys.user, () => getUser(user));
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
-    // TODO: update the user in the query cache
+    queryClient.setQueryData(queryKeys.user, newUser);
   }
 
   // meant to be called from useAuth
   function clearUser() {
-    // TODO: reset user to null in query cache
+    queryClient.setQueryData(queryKeys.user, null);
   }
 
   return { user, updateUser, clearUser };
