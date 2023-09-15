b872530f3872923d598052ca90851b143e26a157
bibiboy <barnesquiat@gmail.com>
2023-09-15 22:25:48 +0900

    [L][Udemy-RQ][C08] 42. 커스텀 변이 훅: useReserveAppointments

--- Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/react-query/queryClient.ts
+++ Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/react-query/queryClient.ts
@@ -25,5 +25,8 @@ export const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
       refetchOnReconnect: false,
     },
+    mutations: {
+      onError: queryErrorHandler,
+    },
   },
 });
