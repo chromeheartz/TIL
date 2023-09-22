8f433c1767836e1d420cd1ee9caac03c7175c069
bibiboy <barnesquiat@gmail.com>
2023-09-22 22:09:17 +0900

    [L][Udemy-RQ][C08] 49.쿼리 "취소 가능하게" 만들기

--- Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -10,7 +10,10 @@ import {
   setStoredUser,
 } from '../../../user-storage';
 
-async function getUser(user: User | null): Promise<User | null> {
+async function getUser(
+  user: User | null,
+  signal: AbortSignal,
+): Promise<User | null> {
   if (!user) return null;
   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
     `/user/${user.id}`,
@@ -29,16 +32,20 @@ interface UseUser {
 
 export function useUser(): UseUser {
   const queryClient = useQueryClient();
-  const { data: user } = useQuery(queryKeys.user, () => getUser(user), {
-    initialData: getStoredUser,
-    onSuccess: (received: User | null) => {
-      if (!received) {
-        clearStoredUser();
-      } else {
-        setStoredUser(received);
-      }
+  const { data: user } = useQuery(
+    queryKeys.user,
+    ({ signal }) => getUser(user, signal),
+    {
+      initialData: getStoredUser,
+      onSuccess: (received: User | null) => {
+        if (!received) {
+          clearStoredUser();
+        } else {
+          setStoredUser(received);
+        }
+      },
     },
-  });
+  );
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
     queryClient.setQueryData(queryKeys.user, newUser);
