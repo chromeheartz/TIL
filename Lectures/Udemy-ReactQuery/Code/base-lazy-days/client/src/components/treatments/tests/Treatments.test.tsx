379dfb97dbe56daec345fdf3c4cc47e42c8c285d
bibiboy <barnesquiat@gmail.com>
2023-09-29 22:09:49 +0900

    [L][Udemy-RQ][C08] 54. 테스트에서의 쿼리 클라이언트와 제공자

--- Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -1,7 +1,8 @@
 import { screen } from '@testing-library/react';
+import { renderWithQueryCient } from 'test-utils';
 
 import { Treatments } from '../Treatments';
 
 test('renders response from query', () => {
-  // write test here
+  renderWithQueryCient(<Treatments />);
 });
--- Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/test-utils/index.tsx
+++ Lectures/Udemy-ReactQuery/Code/base-lazy-days/client/src/test-utils/index.tsx
@@ -1,7 +1,25 @@
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
+import { ReactElement } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+// make a function to generate a unique query client for each test
+const generateQueryClient = () => {
+  return new QueryClient();
+};
+
+export function renderWithQueryCient(
+  ui: ReactElement,
+  client?: QueryClient,
+): RenderResult {
+  const queryClient = client ?? generateQueryClient();
+
+  return render(
+    <QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>,
+  );
+}
+
+// **FOR TESTING CUSTOM HOOKS **
 // import { defaultQueryClientOptions } from '../react-query/queryClient';
-
 // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
 // export const createQueryClientWrapper = (): React.FC => {
 //   const queryClient = generateQueryClient();
