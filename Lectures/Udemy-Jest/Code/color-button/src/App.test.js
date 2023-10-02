4256485f2c13501e530853f4cdac1aed0afad2e1
bibiboy <barnesquiat@gmail.com>
2023-10-02 17:47:19 +0900

    [L][Udemy-Jest][C02] 01. Color Button 앱 시작하기

--- Lectures/Udemy-Jest/Code/color-button/src/App.test.js
+++ Lectures/Udemy-Jest/Code/color-button/src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByRole('link', { name: /learn react/i });
-  expect(linkElement).toBeInTheDocument();
+  // 버튼 여부 / 텍스트가 'Change to Blue'인지 확인
+  const colorButton = screen.getByRole('button', { name: 'Change to Blue' })
+
+  // 배경색이 빨간색인지 확인
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+});
+// test('button has correct initial text', () => {});
+test('button turns blue when clicked', () => {
+
 });
