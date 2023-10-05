8da569378fb98b3c78ef75bdcfcef8f5933ef061
bibiboy <barnesquiat@gmail.com>
2023-10-05 16:25:08 +0900

    [L][Udemy-Jest][C02] 11. 코드 퀴즈

--- Lectures/Udemy-Jest/Code/color-button/src/App.js
+++ Lectures/Udemy-Jest/Code/color-button/src/App.js
@@ -7,10 +7,10 @@ export function replaceCamelWithSpaces(colorName) {
 }
 
 function App() {
-  const [buttonColor, setButtonColor] = useState("red");
+  const [buttonColor, setButtonColor] = useState("MediumVioletRed");
   const [disabled, setdisabled] = useState(false);
 
-  const newButtonColor = buttonColor === "red" ? "blue" : "red";
+  const newButtonColor = buttonColor === "MediumVioletRed" ? "MidnightBlue" : "MediumVioletRed";
 
   return (
     <div>
@@ -19,7 +19,7 @@ function App() {
         onClick={() => setButtonColor(newButtonColor)}
         disabled={disabled}
       >
-        Change to {newButtonColor}
+        Change to {replaceCamelWithSpaces(newButtonColor)}
       </button>
       <br />
       <input
