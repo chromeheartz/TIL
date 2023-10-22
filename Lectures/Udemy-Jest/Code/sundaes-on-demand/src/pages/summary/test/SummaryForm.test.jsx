7f5b4e093347f6c9c6f0b69f08ade120ac75a9bc
bibiboy <barnesquiat@gmail.com>
2023-10-22 18:32:06 +0900

    [L][Udemy-Jest][C04] 08. 테스트 요소가 페이지에 없음: 팝오버 테스트 시작하기

--- Lectures/Udemy-Jest/Code/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ Lectures/Udemy-Jest/Code/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -31,9 +31,23 @@ test("Checkbox enables button on first click and disables on second click", asyn
 test("popover responds to hover", async () => {
   const user = userEvent.setup();
 
+  render(<SummaryForm />);
+
   // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will be actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
 
   // popover appears on mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.queryByText(
+    /no ice cream will be actually be delivered/i
+  );
+  expect(popover).toBeInTheDocument();
 
   // popover disappears when we mouse out
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
 });
