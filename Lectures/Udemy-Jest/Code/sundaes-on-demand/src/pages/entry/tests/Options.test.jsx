6cbe6cebb5e625f2b181a5c51ee579c5c68bd77e
bibiboy <barnesquiat@gmail.com>
2023-10-30 20:56:36 +0900

    [L][Udemy-Jest][C05] 05. 코드 퀴즈

--- Lectures/Udemy-Jest/Code/sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
+++ Lectures/Udemy-Jest/Code/sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
@@ -13,3 +13,20 @@ test("displays image for each scoop option from server", async () => {
   const altText = scoopImages.map(element => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("Displays image for each toppings option from server", async () => {
+  // Mock Service Worker will return three toppings from server
+  render(<Options optionType="toppings" />);
+  // find images, expect 3 based on what msw returns
+  const images = await screen.findAllByRole("img", { name: /topping$/i });
+  expect(images).toHaveLength(3);
+
+  // check the actual alt text for the images
+  // @ts-ignore
+  const imagesTitles = images.map(img => img.alt);
+  expect(imagesTitles).toStrictEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
