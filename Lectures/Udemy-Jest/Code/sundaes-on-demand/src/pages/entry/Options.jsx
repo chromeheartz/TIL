86c4e4f2cc2eb15894a748ead5f9aace8286a2b7
bibiboy <barnesquiat@gmail.com>
2023-11-05 18:01:28 +0900

    [L][Udemy-Jest][C05] React Code

--- Lectures/Udemy-Jest/Code/sundaes-on-demand/src/pages/entry/Options.jsx
+++ Lectures/Udemy-Jest/Code/sundaes-on-demand/src/pages/entry/Options.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
+import AlertBanner from "../common/AlertBanner";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   // optionType is 'scoops' or 'toppings
   useEffect(() => {
@@ -14,8 +16,13 @@ export default function Options({ optionType }) {
       .then(response => setItems(response.data))
       .catch(error => {
         // TODO: handle error response
+        setError(true);
       });
   }, [optionType]);
+
+  if (error) {
+    <AlertBanner />;
+  }
 
   // TODO: replace `null` with ToppingOption when available
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
