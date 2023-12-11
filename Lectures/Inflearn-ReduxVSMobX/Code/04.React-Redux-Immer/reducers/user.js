96c224c59e8f70e0c2a42acdef683b59b7d9888b
chromeheartz <barnesquiat@gmail.com>
2023-12-11 15:07:18 +0900

    [L][Inflearn-ReduxVSMobX][C02] 08. Immer

--- Lectures/Inflearn-ReduxVSMobX/Code/04.React-Redux-Immer/reducers/user.js
+++ Lectures/Inflearn-ReduxVSMobX/Code/04.React-Redux-Immer/reducers/user.js
@@ -1,35 +1,41 @@
+const produce = require("immer");
+
 const initialState = {
   isLoggingIn: false,
   data: null,
 };
 
+// nextState = produce(prevState, (draft) => {})
+
 const userReducer = (prevState = initialState, action) => {
-  switch (action.type) {
-    case "LOG_IN_REQUEST":
-      return {
-        ...prevState,
-        isLoggingIn: true,
-      };
-    case "LOG_IN_SUCCESS":
-      return {
-        ...prevState,
-        data: action.data,
-        isLoggingIn: false,
-      };
-    case "LOG_IN_FAILURE":
-      return {
-        ...prevState,
-        data: null,
-        isLoggingIn: false,
-      };
-    case "LOG_OUT":
-      return {
-        ...prevState,
-        data: null,
-      };
-    default:
-      return prevState;
-  }
+  return produce(prevState, (draft) => {
+    switch (action.type) {
+      case "LOG_IN_REQUEST":
+        return {
+          ...prevState,
+          isLoggingIn: true,
+        };
+      case "LOG_IN_SUCCESS":
+        return {
+          ...prevState,
+          data: action.data,
+          isLoggingIn: false,
+        };
+      case "LOG_IN_FAILURE":
+        return {
+          ...prevState,
+          data: null,
+          isLoggingIn: false,
+        };
+      case "LOG_OUT":
+        return {
+          ...prevState,
+          data: null,
+        };
+      default:
+        return prevState;
+    }
+  });
 };
 
 module.exports = userReducer;
