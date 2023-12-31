6a3664b2a9a91711b9e7ac64ff70bacf6aac3792
chromeheartz <barnesquiat@gmail.com>
2023-12-31 13:49:47 +0900

    [L][Inflearn-ReduxVSMobX][C03] 05. 리덕스를 쓰지 말아야할 때 (인풋)

--- Lectures/Inflearn-ReduxVSMobX/Code/05.Redux-Toolkit/actions/user.js
+++ Lectures/Inflearn-ReduxVSMobX/Code/05.Redux-Toolkit/actions/user.js
@@ -8,7 +8,7 @@ const delay = (time, value) =>
   });
 
 exports.logIn = createAsyncThunk("user/logIn", async (data, thunkAPI) => {
-  throw new Error("비밀번호가 틀렸습니다.");
+  // throw new Error("비밀번호가 틀렸습니다.");
   return await delay(500, {
     userId: 1,
     nickname: "bibi",
