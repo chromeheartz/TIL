fdb3c97a9e50e295bbe1ff18b4ebee962cdb0b9b
chromeheartz <barnesquiat@gmail.com>
2024-01-01 15:17:34 +0900

    [L][Inflearn-ReduxVSMobX][C03] 05. 리덕스를 쓰지 말아야할 때 (인풋)

--- Lectures/Inflearn-ReduxVSMobX/Code/05.Redux-Toolkit/App.jsx
+++ Lectures/Inflearn-ReduxVSMobX/Code/05.Redux-Toolkit/App.jsx
@@ -9,6 +9,8 @@ const userSlice = require("./reducers/userSlice");
 const App = () => {
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const onClick = useCallback(() => {
     dispatch(
@@ -31,6 +33,28 @@ const App = () => {
       })
     );
   }, []);
+
+  const onChangeEmail = useCallback((e) => {
+    setEmail(e.target.value);
+  }, []);
+
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.password);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      dispatch(
+        userSlice.actions.setLoginForm({
+          email,
+          password,
+        })
+      );
+    },
+    [dispatch, email, password]
+  );
+
   return (
     <div>
       {user.isLoggingIn ? (
@@ -46,6 +70,10 @@ const App = () => {
         <button onClick={onLogout}>로그아웃</button>
       )}
       <button onClick={onAddPost}>게시글 작성</button>
+      <form onSubmit={onSubmit}>
+        <input type="email" value={email} onChange={onChangeEmail} />
+        <input type="password" value={password} onChange={onChangePassword} />
+      </form>
     </div>
   );
 };
