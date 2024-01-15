3ab03739419366f85b8fdd3a3cf0cdff9f789b91
chromeheartz <barnesquiat@gmail.com>
2024-01-15 12:54:04 +0900

    [L][Inflearn-ReduxVSMobX][C04] 03. 리덕스와의 차이점과 Q&A

--- Lectures/Inflearn-ReduxVSMobX/Code/07.mobxQA/index.js
+++ Lectures/Inflearn-ReduxVSMobX/Code/07.mobxQA/index.js
@@ -1,10 +1,17 @@
 const { observable, autorun, runInAction, reaction } = require("mobx");
 
-const state = observable({
-  compA: "a",
-  compB: 12,
-  compC: null,
+const userState = observable({
+  isLogginIn: true,
+  data: null,
 });
+
+const postState = observable([]);
+
+postState.push({ id: 1, content: "hello world" });
+userState.data = {
+  id: 1,
+  nickname: "bibi",
+};
 
 autorun(() => {
   console.log("changed", state.compA);
