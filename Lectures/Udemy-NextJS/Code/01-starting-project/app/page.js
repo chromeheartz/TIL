829d6c7ff50e86298a165db36dd6832e81e277b6
chromeheartz <barnesquiat@gmail.com>
2024-07-23 10:49:22 +0900

    [NextJS][01] Code : 01-06 ~ 01-10

--- Lectures/Udemy-NextJS/Code/01-starting-project/app/page.js
+++ Lectures/Udemy-NextJS/Code/01-starting-project/app/page.js
@@ -1,9 +1,16 @@
+import Link from "next/link";
+import Header from "@/components/header";
+
 export default function Home() {
   return (
     <main>
+      <Header />
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <Link href="/about" className="my-link">
+          About Us
+        </Link>
+      </p>
     </main>
   );
 }
