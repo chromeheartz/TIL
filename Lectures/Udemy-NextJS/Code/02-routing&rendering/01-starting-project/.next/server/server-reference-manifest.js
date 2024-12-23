5eb361437c896615cee20b15e3a968abd1a80e66
chromeheartz <barnesquiat@gmail.com>
2024-12-23 10:52:47 +0900

    [Next][02] 01 project code update

---
+++ Lectures/Udemy-NextJS/Code/02-routing&rendering/01-starting-project/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"RNic8bbnN04LMQtWQzISjMq9BTi7V8Dht/YEhG2kGpk=\"\n}"
---
+++ Lectures/Udemy-NextJS/Code/02-routing&rendering/01-starting-project/app/archive/@latest/page.js
@@ -0,0 +1,3 @@
+export default function LatestNewsPage() {
+  return <h1>latest news page</h1>;
+}
