58b7a733b8027e57e75dbbcc530d435dc6106638
chromeheartz <barnesquiat@gmail.com>
2025-01-06 17:19:49 +0900

    [Next][02] 01 project code update

---
+++ Lectures/Udemy-NextJS/Code/02-routing&rendering/01-starting-project/app/archive/@archive/[[...filter]]/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function FilterError({ error }) {
+  return (
+    <div id="error">
+      <h2>An error occured!</h2>
+      <p>{error.message}</p>
+    </div>
+  );
+}
--- Lectures/Udemy-NextJS/Code/02-routing&rendering/01-starting-project/app/archive/@archive/[[...filter]]/page.js
+++ Lectures/Udemy-NextJS/Code/02-routing&rendering/01-starting-project/app/archive/@archive/[[...filter]]/page.js
@@ -1,25 +1,63 @@
 import NewsList from "@/components/news-list";
-import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
 import Link from "next/link";
 
 export default function FilteredNewsPage({ params }) {
   const filter = params.filter;
-  console.log(filter);
-  const links = getAvailableNewsYears();
-  // const news = getNewsForYear(newsYear);
-  return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {links.map((link) => (
-            <li key={link}>
-              <Link href={`/archive/${link}`}>{link}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  );
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
 
-  // return <NewsList news={news} />;
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No News found for the selected period.</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  if (
+    (selectedYear && !getAvailableNewsYears().includes(+selectedYear)) ||
+    (selectedMonth &&
+      !getAvailableNewsMonths(selectedYear).includes(+selectedMonth))
+  ) {
+    throw new Error("Invalid filter.");
+  }
+
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
 }
