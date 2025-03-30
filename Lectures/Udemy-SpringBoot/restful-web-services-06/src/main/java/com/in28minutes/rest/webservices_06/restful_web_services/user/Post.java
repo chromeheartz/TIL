a6266b19ab857fb2b4883b0f07bcc3dbafe41b29
chromeheartz <barnesquiat@gmail.com>
2025-03-30 13:13:42 +0900

    [Spring][04] 04. Spring Boot의 목표 이해하기

---
+++ Lectures/Udemy-SpringBoot/restful-web-services-06/src/main/java/com/in28minutes/rest/webservices_06/restful_web_services/user/Post.java
@@ -0,0 +1,45 @@
+package com.in28minutes.rest.webservices_06.restful_web_services.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Post {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String description;
+	
+	@ManyToOne
+	@JsonIgnore
+	private User user;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Post [id=" + id + ", description=" + description + "]";
+	}
+	
+	
+}
