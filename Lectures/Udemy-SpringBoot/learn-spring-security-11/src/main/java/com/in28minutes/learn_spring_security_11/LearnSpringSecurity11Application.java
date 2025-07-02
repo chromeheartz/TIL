d5147d61fb80a5505f003fa293e95bf2e039fce8
chromeheartz <barnesquiat@gmail.com>
2025-07-02 11:15:22 +0900

    [Spring][10] section 11 code update

---
+++ Lectures/Udemy-SpringBoot/learn-spring-security-11/src/main/java/com/in28minutes/learn_spring_security_11/LearnSpringSecurity11Application.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learn_spring_security_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringSecurity11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringSecurity11Application.class, args);
+	}
+
+}
---
+++ Lectures/Udemy-SpringBoot/learn-spring-security-11/src/test/java/com/in28minutes/learn_spring_security_11/LearnSpringSecurity11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learn_spring_security_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringSecurity11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
