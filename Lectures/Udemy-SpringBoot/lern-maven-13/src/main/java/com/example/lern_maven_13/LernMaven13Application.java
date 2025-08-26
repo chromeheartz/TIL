4f1437c9cd9bd9edbb8f97129395fb2f99e63b15
chromeheartz <barnesquiat@gmail.com>
2025-08-26 11:04:42 +0900

    [Spring][13] maven code update

---
+++ Lectures/Udemy-SpringBoot/lern-maven-13/src/main/java/com/example/lern_maven_13/LernMaven13Application.java
@@ -0,0 +1,13 @@
+package com.example.lern_maven_13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LernMaven13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LernMaven13Application.class, args);
+	}
+
+}
