ef29f99b938dc11de24729140fc3e7641244389d
chromeheartz <barnesquiat@gmail.com>
2025-08-27 15:47:27 +0900

    [Spring][14] gradle code update

---
+++ Lectures/Udemy-SpringBoot/learn-gradle-14/src/test/java/com/in28minutes/learn_gradle_14/LearnGradle14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learn_gradle_14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGradle14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
