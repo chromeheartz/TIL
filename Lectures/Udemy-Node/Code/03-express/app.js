051f3dc6576e78fcafe0634db77d337d55669a21
chromeheartz <barnesquiat@gmail.com>
2026-01-08 10:55:49 +0900

    [Node][03] node code update

---
+++ Lectures/Udemy-Node/Code/03-express/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("middle ware called");
+  next();
+});
+app.use("/add-product", (req, res, next) => {
+  console.log("In another middleware!");
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use("/", (req, res, next) => {
+  console.log("In another middleware!");
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+app.listen(3000);
